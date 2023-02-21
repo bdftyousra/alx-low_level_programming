88c14b3524a7e4d8f2091d535c31e4813ba682b2
bdftyousra <bdftyousra@gmail.com>
2023-02-21 15:12:36 -0800

    c programming

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isalpha - checks for alphabetic character
+ * @c: the character to be checked
+ * Description: 'the program's description'
+ * Return: 1 if c is a letter, 0 otherwise
+ */
+
+
+int _isalpha(int c)
+{
+	return ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'));
+}
