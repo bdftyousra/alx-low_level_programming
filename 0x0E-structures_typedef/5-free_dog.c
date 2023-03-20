fd1e1c2bbffbe928d051f2169bfcd8a9893272fe
bdftyousra <bdftyousra@gmail.com>
2023-03-20 10:20:36 -0700

    0x0E-structures_typedef

--- 0x0E-structures_typedef/5-free_dog.c
+++ 0x0E-structures_typedef/5-free_dog.c
@@ -1,3 +1,4 @@
+#include <stdlib.h>
 #include "dog.h"
 
 /**
@@ -7,10 +8,10 @@
 */
 void free_dog(dog_t *d)
 {
-if (d == NULL)
-return;
-
-free(d->name);
-free(d->owner);
-free(d);
+	if (d)
+	{
+		free(d->name);
+		free(d->owner);
+		free(d);
+	}
 }
