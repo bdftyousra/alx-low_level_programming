247458a3baba2b6ce464d2b82d66e21d2f5ecff1
bdftyousra <bdftyousra@gmail.com>
2023-05-22 12:34:12 -0700

    m

---
+++ 0x18-dynamic_libraries/100-operations.c
@@ -0,0 +1,60 @@
+
+/**
+ * add - Computes the sum of two numbers
+ * @left: The left-sided number
+ * @right: The right-sided number
+ *
+ * Return: The sum of the two numbers
+ */
+int add(int left, int right)
+{
+	return (left + right);
+}
+
+/**
+ * sub - Computes the difference of two numbers
+ * @left: The left-sided number
+ * @right: The right-sided number
+ *
+ * Return: The difference of the two numbers
+ */
+int sub(int left, int right)
+{
+	return (left - right);
+}
+
+/**
+ * mul - Computes the product of two numbers
+ * @left: The left-sided number
+ * @right: The right-sided number
+ *
+ * Return: The product of the two numbers
+ */
+int mul(int left, int right)
+{
+	return (left * right);
+}
+
+/**
+ * div - Computes the quotient of two numbers
+ * @left: The left-sided number
+ * @right: The right-sided number
+ *
+ * Return: The quotient of the two numbers
+ */
+int div(int left, int right)
+{
+	return (left / right);
+}
+
+/**
+ * mod - Computes the modulo of two numbers
+ * @left: The left-sided number
+ * @right: The right-sided number
+ *
+ * Return: The modulo of the two numbers
+ */
+int mod(int left, int right)
+{
+	return (left % right);
+}
